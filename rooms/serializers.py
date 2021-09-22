63171d8cd6bd469fd448efe4a656742190ab079e
Paek Seungwoo <paekseungwoo@Paekui-Macmini.local>
2021-09-22 15:59:46 +0900

    #2.0 ListRoomsView & SeeRoomView

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,19 +1,13 @@
 from django.db.models import fields
 from rest_framework import serializers
-from users.serializers import TinyUserSerializer
+from users.serializers import UserSerializer
 from .models import Room
 
 
 class RoomSerializer(serializers.ModelSerializer):
 
-    user = TinyUserSerializer()
+    user = UserSerializer()
 
     class Meta:
         model = Room
-        fields = ("pk", "name", "price", "instant_book", "user")
-
-
-class BigRoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Room
-        fields = "__all__"
+        exclude = ("modified",)
